692747bc7428085c30e2089d663b641f553f05a3
Volkan Unsal <spocksplanet@gmail.com>
2019-05-17 11:28:48 -0400

    Remove commented out casts

--- src/casts/casts.sql
+++ src/casts/casts.sql
@@ -8,7 +8,6 @@ CREATE CAST (TEXT AS _rrule.RRULESET)
   AS IMPLICIT;
 
 
-
 CREATE CAST (jsonb AS _rrule.RRULE)
   WITH FUNCTION _rrule.jsonb_to_rrule(jsonb)
   AS IMPLICIT;
@@ -17,23 +16,4 @@ CREATE CAST (jsonb AS _rrule.RRULE)
 CREATE CAST (_rrule.RRULE AS jsonb)
   WITH FUNCTION _rrule.rrule_to_jsonb(_rrule.RRULE)
   AS IMPLICIT;
-
-
--- CREATE CAST (jsonb AS _rrule.RRULESET)
---   WITH FUNCTION _rrule.jsonb_to_rruleset(jsonb)
---   AS IMPLICIT;
-
--- CREATE CAST (_rrule.RRULESET AS jsonb)
---   WITH FUNCTION _rrule.rruleset_to_jsonb(_rrule.RRULESET)
---   AS IMPLICIT;
-
-
--- CREATE CAST (jsonb AS _rrule.RRULESET[])
---   WITH FUNCTION _rrule.jsonb_to_rruleset_array(jsonb)
---   AS IMPLICIT;
-
-
--- CREATE CAST (_rrule.RRULESET[] AS jsonb)
---   WITH FUNCTION _rrule.rruleset_array_to_jsonb(_rrule.RRULESET[])
---   AS IMPLICIT;
 
