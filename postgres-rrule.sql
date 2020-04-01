37b38f360d7ed943f4e556faa9b2da9c96c4e185
GavinRay97 <ray.gavin97@gmail.com>
2020-04-01 16:33:57 -0400

    Add EXDATE and RDATE parsing to compiled master SQL file

--- postgres-rrule.sql
+++ postgres-rrule.sql
@@ -429,14 +429,16 @@ CREATE OR REPLACE FUNCTION _rrule.rruleset (TEXT)
 RETURNS _rrule.RRULESET AS $$
   WITH "dtstart-line" AS (SELECT _rrule.parse_line($1::text, 'DTSTART') as "x"),
   "dtend-line" AS (SELECT _rrule.parse_line($1::text, 'DTEND') as "x"),
-  "exrule-line" AS (SELECT _rrule.parse_line($1::text, 'EXRULE') as "x")
+  "exrule-line" AS (SELECT _rrule.parse_line($1::text, 'EXRULE') as "x"),
+  "rdate-line" AS (SELECT _rrule.parse_line($1::text, 'RDATE') as "x"),
+  "exdate-line" AS (SELECT _rrule.parse_line($1::text, 'EXDATE') as "x")
   SELECT
     (SELECT "x"::timestamp FROM "dtstart-line" LIMIT 1) AS "dtstart",
     (SELECT "x"::timestamp FROM "dtend-line" LIMIT 1) AS "dtend",
     (SELECT _rrule.rrule($1::text) "rrule") as "rrule",
     (SELECT _rrule.rrule("x"::text) "rrule" FROM "exrule-line") as "exrule",
-    NULL::TIMESTAMP[] "rdate",
-    NULL::TIMESTAMP[] "exdate";
+    (SELECT (regexp_split_to_array("x"::text, ','))::TIMESTAMP[] from "rdate-line" AS "rdate"),
+    (SELECT (regexp_split_to_array("x"::text, ','))::TIMESTAMP[] from "exdate-line" AS "exdate");
 $$ LANGUAGE SQL IMMUTABLE STRICT;
 
 -- All of the function(rrule, ...) forms also accept a text argument, which will
@@ -983,4 +985,3 @@ CREATE CAST (jsonb AS _rrule.RRULE)
 CREATE CAST (_rrule.RRULE AS jsonb)
   WITH FUNCTION _rrule.rrule_to_jsonb(_rrule.RRULE)
   AS IMPLICIT;
-
